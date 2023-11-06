da7058cb45dac0d5e24c9d33089cc75607057b67
Dilanka <68213182+DSDiL@users.noreply.github.com>
2023-11-06 21:29:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './style.css'
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route path='/' element={<Home/>}/>
         </Routes>
