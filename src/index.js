eb4fc9daa8c30a16f5ae8a29b849bb8d9b58ab87
Dilanka <68213182+DSDiL@users.noreply.github.com>
2023-11-06 20:40:47 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,25 +1,6 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import {
-  BrowserRouter,
-  Route,
-  Routes,
-} from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-import './style.css'
-import Home from './home'
-
-const App = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<Home/>} path="/" />
-      </Routes>
-    </BrowserRouter>
-  )
-}
-
-const dom = document.getElementById('app');
-const root = createRoot(dom);
-
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById('app'));
+root.render( <App /> );
